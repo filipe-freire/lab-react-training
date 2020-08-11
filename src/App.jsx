bb5c56d658447b34557535885e3bc1cda1418f35
Filipe Freire <filipefreire@protonmail.com>
2020-08-11 15:02:32 +0100

    Complete Iteration 9

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import CreditCard from './components/CreditCard';
 import Rating from './components/Rating';
 import DriverCard from './components/DriverCard';
 import LikeButton from './components/LikeButton';
+import ClickablePicture from './components/ClickablePicture';
 
 function App() {
   return (
@@ -99,6 +100,10 @@ function App() {
         }}
       />
       <LikeButton /> <LikeButton />
+      <ClickablePicture
+        img="/img/persons/maxence.png"
+        imgClicked="/img/persons/maxence-glasses.png"
+      />
     </div>
   );
 }
---
+++ src/components/ClickablePicture.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class ClickablePicture extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: false,
+    };
+  }
+
+  handleLikeButtonClick = () => {
+    this.setState(() => {
+      return {
+        value: !this.state.value,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <img
+        src={this.state.value ? this.props.imgClicked : this.props.img}
+        alt=""
+        onClick={this.handleLikeButtonClick}
+        style={{ width: '100px' }}
+      ></img>
+    );
+  }
+}
+
+export default ClickablePicture;
